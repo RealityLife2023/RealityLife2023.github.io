0a0ff072ae406ba3c6b0bcd5a9665c0058bd0db4
umi <asiessoyeltier1@gmail.com>
2024-08-02 12:42:50 -0500

    [fixbug] Not managed context for class's members

--- src/record.js
+++ src/record.js
@@ -1,3 +1,203 @@
+
+let tool = {
+   fileName : undefined,
+   teller   : undefined,
+   submitButton   : undefined,
+   recordButton   : undefined,
+   stopButton     : undefined,
+   mediaRecorder  : undefined,
+   videoScreen    : undefined,
+   video          : undefined,
+}
+
+/*
+* Request all media disposable, this assumes that the microphone and camera are needed
+* @returns {DevicesMediaStream} stream from microphone and camera
+*/
+async function resolveMedia()
+{
+   let constraints = { "video": true, "audio": true};
+	
+   return await navigator.mediaDevices.getUserMedia(constraints);
+}
+
+
+/*
+* Fetch a JSON with the params to POST a file at the S3 bucket
+* 
+* @param {object} fileMetada - Name and type of the file
+*/
+async function getPresignedUrl(fileMedata)
+{
+   let url = "https://servicenuruk.realitynear.org:7726/presigned";
+
+   let request = 
+      {
+         method  : "POST",
+         headers: {
+            "Content-Type" : "application/json",
+            "authorization" : token, // <- Global token defined in the registrationForms
+         },
+         body    : JSON.stringify(fileMedata),
+      };
+
+   return fetch(url, request).then((response) => response.json());
+}
+
+
+/**
+ * Start recording with audio and camera streams
+ */
+async function recordVideo( event )
+{
+   event.preventDefault();
+
+   if(tool.fileName === "" || tool.fileName.length < 2)
+   {
+      tool.teller.textContent = "El nombre del archivo no es válido";
+      return;
+   }
+
+   // Get the microphone and camera
+   tool.teller.textContent = "Accediendo al micrófono y cámara";
+   
+   tool.teller.textContent = "Cámara y microfono listo";
+
+   // Wait 2 seconds
+   tool.teller.textContent = "Grabando";
+
+   tool.videoScreen.srcObject = stream;
+
+   let stream = await resolveMedia();
+   tool.mediaRecorder = new MediaRecorder(stream, { mimeType: "video/webm"});
+
+   tool.mediaRecorder.addEventListener("dataavailable", ( event ) => {tool.video = event});
+
+   // Start the recording
+   try
+   {
+      // Disable the record button
+      tool.recordButton.disabled = true;
+      tool.stopButton.disabled = false;
+
+      tool.mediaRecorder.start();
+      startTimer();
+   }
+   catch( error )
+   {
+      tool.teller.textContent = "Algo salio mal, intenta darle al boton de grabar otra vez o revisa que la camara y el microfono esten conectados";
+
+      console.log( error );
+      // Habilitate the record button
+      tool.recordButton.disabled = false;
+   }
+}
+
+/* Stops all the streams and calls inderectely a callback for MediaStream*/
+async function storeRecord( event )
+{
+   event.preventDefault();
+
+   tool.teller.textContent = "Guardaremos tu archivo";
+
+   tool.mediaRecorder.stop();
+
+   stopTimer();
+
+   // Stop the MediaStreamTrack of every device
+   tool.videoScreen.srcObject.getTracks().forEach( (track) => track.stop() );
+
+   tool.submitButton.disabled = false;
+
+   // Clean the video screen
+   tool.videoScreen.pause();
+
+   tool.videoScreen.removeAttribute("src");
+
+   tool.videoScreen.load();
+}
+
+/**
+ * 
+ * Captures click event of submit button and expects the tool to have the video data stored
+ * 
+ * @param {Event} event 
+ */
+async function saveToRemoteDisk( event )
+{
+   event.preventDefault();
+
+   let blob = new Blob([tool.video.data], { type : "video/webm" });
+
+   let date = new Date();
+
+   console.assert(tool.fileName.value.length != 0);
+
+   let metadata = {
+
+      // Sanitize the name and does not go with extension
+      name : `${tool.fileName.value}`,
+      mimetype : "video/webm",
+      extension : "webm",
+      // Take the date in ISO 8601 with the local time
+      date : new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString(),
+      // Take the encoding
+      encoding : "binary",
+      // Weight in bytes
+      weight : blob.size,
+   };
+
+   console.debug(metadata);
+
+   // Request the presigned url for the post
+   let params = await getPresignedUrl(metadata);
+
+   let body = new FormData();
+
+   Object.keys(params.fields).forEach( key => { body.append(key, params.fields[key]) });
+
+   body.append("file", blob);
+
+   let request = 
+      {
+         method  : "POST",
+         mode    : "no-cors",
+         body    : body,
+      };
+
+   await fetch(params.url, request).then( (response) => {
+      // Debug the result
+      console.debug(response);
+      tool.teller.textContent = "Video subido con exito";
+
+      dashboard.refresh();
+
+   }).catch( (error) => 
+   {
+      console.log(error);
+      tool.teller.textContent = "Algo a ocurrido vuelve porfavor a intentar subirlo";
+
+      tool.submitButton.disabled = false;
+   });
+}
+
+function saveToDisk( event )
+{
+   let invisibleAnchor = document.getElementById("invisible-anchor__a");
+
+   let blob = new Blob([event.data], { type : "video/webm" });
+
+   let inner_path = URL.createObjectURL(blob);
+
+   invisibleAnchor.setAttribute("href", inner_path);
+
+   invisibleAnchor.setAttribute("download", "generic_video.webm");
+
+   invisibleAnchor.click();
+
+   URL.revokeObjectURL(inner_path);
+}
+
 /*
  * Elements that will serve as tools to record the video
  */
@@ -20,208 +220,25 @@ class VideoRecord extends HTMLElement
          <p id="teller__p"></p>
       `;
 
-      this.teller = this.children[3];
-      this.fileName = this.children[0];
+      tool.teller = this.children[3];
+      tool.fileName = this.children[0];
 
-      this.videoScreen = this.children[1].children[0];
-      this.recordButton = this.children[2].children[0];
-      this.stopButton = this.children[2].children[1];
-      this.submitButton = this.children[2].children[2];
+      tool.videoScreen = this.children[1].children[0];
+      tool.recordButton = this.children[2].children[0];
+      tool.stopButton = this.children[2].children[1];
+      tool.submitButton = this.children[2].children[2];
 
       /* Settings for the elements */
 
-      this.recordButton.onclick = this.recordVideo;
-      this.stopButton.onclick = this.storeRecord;
-      this.submitButton.onclick = this.saveToRemoteDisk;
+      tool.recordButton.onclick = recordVideo;
+      tool.stopButton.onclick = storeRecord;
+      tool.submitButton.onclick = saveToRemoteDisk;
 
-      this.submitButton.disabled = true;
-      this.stopButton.onclick = true;
-   }
-
-   async saveToRemoteDisk()
-   {
-      let blob = new Blob([this.stopEvent.data], { type : "video/webm" });
-
-      let date = new Date();
-
-      console.assert(this.fileName.value.length != 0);
-
-      let metadata = {
-
-         // Sanitize the name and does not go with extension
-         name : `${this.fileName.value}`,
-         mimetype : "video/webm",
-         extension : "webm",
-         // Take the date in ISO 8601 with the local time
-         date : new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString(),
-         // Take the encoding
-         encoding : "binary",
-         // Weight in bytes
-         weight : blob.size,
-      };
-
-      console.debug(metadata);
-
-      // Request the presigned url for the post
-      let params = await this.getPresignedUrl(metadata);
-
-      let body = new FormData();
-
-      Object.keys(params.fields).forEach( key => { body.append(key, params.fields[key]) });
-
-      body.append("file", blob);
-
-      let request = 
-         {
-            method  : "POST",
-            mode    : "no-cors",
-            body    : body,
-         };
-
-      await fetch(params.url, request).then( (response) => {
-         // Debug the result
-         console.debug(response);
-         this.teller.textContent = "Video subido con exito";
-
-         dashboard.refresh();
-
-      }).catch( (error) => 
-      {
-         console.log(error);
-         this.teller.textContent = "Algo a ocurrido vuelve porfavor a intentar subirlo";
-
-         this.submitButton.disabled = false;
-      });
-   }
-
-
-   /*
-   * Fetch a JSON with the params to POST a file at the S3 bucket
-   * 
-   * @param {object} fileMetada - Name and type of the file
-   */
-   async getPresignedUrl(fileMedata)
-   {
-      let url = "https://servicenuruk.realitynear.org:7726/presigned";
-
-      let request = 
-         {
-            method  : "POST",
-            headers: {
-               "Content-Type" : "application/json",
-               "authorization" : token, // <- Global token defined in the registrationForms
-            },
-            body    : JSON.stringify(fileMedata),
-         };
-
-      return fetch(url, request).then((response) => response.json());
-   }
-
-   captureMediaEvent( event )
-   {
-      this.stopEvent = event;
-   }
-
-   /**
-    * Start recording with audio and camera streams
-    */
-   async recordVideo()
-   {
-      if(this.fileName === "" || this.fileName.length < 2)
-      {
-         this.teller.textContent = "El nombre del archivo no es válido";
-         return;
-      }
-
-      // Get the microphone and camera
-      this.teller.textContent = "Accediendo al micrófono y cámara";
-      
-      this.teller.textContent = "Cámara y microfono listo";
-
-      // Wait 2 seconds
-      this.teller.textContent = "Grabando";
-
-      this.videoScreen.srcObject = stream;
-
-      let stream = await resolveMedia();
-      this.mediaRecorder = new MediaRecorder(stream, { mimeType: "video/webm"});
-
-      this.mediaRecorder.addEventListener("dataavailable", captureMediaEvent);
-
-      // Start the recording
-      try
-      {
-         // Disable the record button
-         this.recordButton.disabled = true;
-         this.stopButton.disabled = false;
-
-         this.mediaRecorder.start();
-         startTimer();
-      }
-      catch( error )
-      {
-         this.teller.textContent = "Algo salio mal, intenta darle al boton de grabar otra vez o revisa que la camara y el microfono esten conectados";
-
-         console.log( error );
-         // Habilitate the record button
-         this.recordButton.disabled = false;
-      }
-   }
-
-   /* Stops all the streams and calls inderectely a callback for MediaStream*/
-   async storeRecord()
-   {
-      this.teller.textContent = "Guardaremos tu archivo";
-
-      this.mediaRecorder.stop();
-
-      stopTimer();
-
-      // Stop the MediaStreamTrack of every device
-      this.videoScreen.srcObject.getTracks().forEach( (track) => track.stop() );
-
-      this.submitButton.disabled = false;
-
-      // Clean the video screen
-      this.videoScreen.pause();
-
-      this.videoScreen.removeAttribute("src");
-
-      this.videoScreen.load();
+      tool.submitButton.disabled = true;
+      tool.stopButton.disabled = true;
    }
 }
 
 // Define the custom element right here!
 window.customElements.define("video-record", VideoRecord);
 
-
-/*
-* Request all media disposable, this assumes that the microphone and camera are needed
-* @returns {DevicesMediaStream} stream from microphone and camera
-*/
-async function resolveMedia()
-{
-   let constraints = { "video": true, "audio": true};
-	
-   return await navigator.mediaDevices.getUserMedia(constraints);
-}
-
-
-function saveToDisk( event )
-{
-   let invisibleAnchor = document.getElementById("invisible-anchor__a");
-
-   let blob = new Blob([event.data], { type : "video/webm" });
-
-   let inner_path = URL.createObjectURL(blob);
-
-   invisibleAnchor.setAttribute("href", inner_path);
-
-   invisibleAnchor.setAttribute("download", "generic_video.webm");
-
-   invisibleAnchor.click();
-
-   URL.revokeObjectURL(inner_path);
-}
-
-let main = document.getElementsByTagName("main")[0];
